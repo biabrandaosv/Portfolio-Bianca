08def7e3c9d6c081493509b7f621a0652985e483
biabrandaosv <115591164+biabrandaosv@users.noreply.github.com>
2022-10-24 19:32:20 -0300

    commit incial

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function() {
+
+        if(aba.classList.contains("selecionado")){
+            return;
+        };
+
+        SelecionarAba(aba)
+        MostrarInformacoesDaAba(aba)
+
+       
+    });
+});
+
+function SelecionarAba(aba) {
+
+        const abaSelecionado = document.querySelector(".aba.selecionado");
+        abaSelecionado.classList.remove("selecionado");
+
+        aba.classList.add("selecionado");
+        
+}
+
+function MostrarInformacoesDaAba (aba) {
+          
+    const informacaoSelecionado = document.querySelector(".informacao.selecionado");
+    informacaoSelecionado.classList.remove("selecionado");
+
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado");
+
+}
